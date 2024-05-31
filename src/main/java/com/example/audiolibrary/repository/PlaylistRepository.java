7a193bad12a007ef49bdc2ec9271ffb3ce03df45
darialapadus <127118850+darialapadus@users.noreply.github.com>
2024-05-31 20:28:47 +0300

    final tests

--- src/main/java/com/example/audiolibrary/repository/PlaylistRepository.java
+++ src/main/java/com/example/audiolibrary/repository/PlaylistRepository.java
@@ -1,7 +1,10 @@
 package com.example.audiolibrary.repository;
 
 import com.example.audiolibrary.model.Playlist;
+import com.example.audiolibrary.model.Song;
 import com.example.audiolibrary.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +13,8 @@ import java.util.Optional;
 
 @Repository
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
-    List<Playlist> findByOwner(User owner);
+//    List<Playlist> findByOwner(User owner);
+    Page<Playlist> findByOwner(User owner, Pageable pageable);
     Optional<Playlist> findByIdAndOwner(Long id, User owner);
     Optional<Playlist> findByNameAndOwner(String name, User owner);
 }
